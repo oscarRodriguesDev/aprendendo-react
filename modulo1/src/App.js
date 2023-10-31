c353fdddc916508e42d43b8354abe21e1dcaf32b
OscarDikma <oscardikma@gmail.com>
2023-10-31 17:40:37 -0300

    atualização modulo 1 31-10-2023

--- modulo1/src/App.js
+++ modulo1/src/App.js
@@ -1,60 +1,30 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from "react";
 
-function App(){
-  const [input, setInput] = useState('');
-  const [tarefas, setTarefas] = useState([
-    'Pagar a conta de luz',
-    'Estudar React JS'
-  ]);
+function App() {
+  const [nutri, setNutri] = useState([]);
 
-  useEffect(()=>{
-    const tarefasStorage = localStorage.getItem('@tarefa');
-
-    if(tarefasStorage){
-      setTarefas(JSON.parse(tarefasStorage))
+  useEffect(() => {
+    function loadApi() {
+      let url = "https://sujeitoprogramador.com/rn-api/?api=posts";
+      fetch(url)
+        .then((r) => r.json())
+        .then((json) => {
+          console.log(json);
+          setNutri(json);
+        });
     }
 
+    loadApi();
   }, []);
 
-
-  useEffect(()=>{ 
-      localStorage.setItem('@tarefa', JSON.stringify(tarefas))
-  }, [tarefas]);
-
-
-  function handleRegister(e){
-    e.preventDefault();
-
-    setTarefas([...tarefas, input]);
-    setInput('');
-
+  function returnValue() {
+    return nutri;
   }
-
-  return(
+  return (
     <div>
-      <h1>Cadastrando usuario</h1>
-
-      <form onSubmit={handleRegister}>
-        <label>Nome da tarefa:</label><br/>
-        <input 
-         placeholder="Digite uma tarefa" 
-         value={input}
-         onChange={ (e) => setInput(e.target.value) }
-        /><br/>
-        <button type="submit">Registrar</button>
-      </form>
-
-      <br/><br/>
-
-      <ul>
-        {tarefas.map( tarefa => (
-          <li key={tarefa}>{tarefa}</li>
-        ))}
-      </ul>
-
+      <div> {returnValue}</div>
     </div>
   );
 }
 
 export default App;
-
